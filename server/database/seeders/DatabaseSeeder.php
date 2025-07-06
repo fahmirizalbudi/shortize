25161eeb0db493b6e70e969d36958b448fc3f1f2
Fahmirizal Budi Ramadhan <fahmirizal.dev@gmail.com>
2025-07-06 14:27:49 +0700

    feat(seeder): create role and user seeder

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +16,24 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+//        User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'test@example.com',
+//        ]);
+
+        Role::create([
+            'role' => 'admin'
+        ]);
+
+        Role::create([
+            'role' => 'user'
+        ]);
+
+        User::create([
+           'name' => 'Administrator',
+           'email' => 'admin@example.com',
+           'password' => bcrypt('demo'),
+           'role_id' => 1
         ]);
     }
 }
