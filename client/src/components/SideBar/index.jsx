4e1390b56e393b0d5a4f0cdbc6b871c6bbda20ea
Fahmirizal Budi Ramadhan <fahmirizal.dev@gmail.com>
2025-07-10 09:57:18 +0700

    style: fix spacing

--- client/src/components/SideBar/index.jsx
+++ client/src/components/SideBar/index.jsx
@@ -3,11 +3,11 @@ import {BiHomeAlt2, BiLink, BiUser} from "react-icons/bi";
 import SideBarMenu from "./SideBarMenu/index.jsx";
 import {useState} from "react";
 
-const SideBar = ({ menuActive }) => {
+const SideBar = () => {
     const [sideBarMenuActive, setSideBarMenuActive] = useState("Quick View")
     return (
         <div className="w-75 flex h-full fixed bg-[#f5f5f5] pb-8 px-8 items flex-col font-outfit">
-            <img src={logo} alt="Logo" className="h-fit w-60 relative right-9 bottom-1"/>
+            <img src={logo} alt="Logo" className="h-fit w-60 relative right-9 bottom-2"/>
             <div className="flex w-full flex-col gap-3">
                 <SideBarMenu Icon={BiHomeAlt2} isActive={sideBarMenuActive} title="Quick View" navigate="#" onSelectedMenu={() => setSideBarMenuActive("Quick View")}/>
                 <SideBarMenu Icon={BiUser} isActive={sideBarMenuActive} title="Users" navigate="#" onSelectedMenu={() => setSideBarMenuActive("Users")}/>
